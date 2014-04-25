3fbe8fea9f4007727c8663cbe933783249accd53
sanchayana2007 <sanchayana2007@gmail.com>
2014-04-25 17:01:54 +0530

    stuck at session

--- Express/app.js
+++ Express/app.js
@@ -2,8 +2,8 @@
 * Module dependencies.
 */
 var express = require('express')
-, routes = require('./routes');
-var app = module.exports = express.createServer();
+var routes = require('./routes');
+var app  = express();
 
 // Express Configuration
 app.configure(function(){
@@ -13,10 +13,20 @@ app.configure(function(){
 		// Express used the Following Middle wire (parser, methodOveride(to Use , DEL, PUT ))
 		app.use(express.bodyParser());
 		app.use(express.methodOverride());
+		//Session maintainence stuffs
+		app.use(express.cookieParser('my secret string'));
+		app.use(express.session({
+				secret: 'my secret string',
+				maxAge: 3600000
+		}));
 		// Express Uses router middle wire to map all the request to proper Listener
 		app.use(app.router);
 		// all the static data should be kept in this 
 		app.use(express.static(__dirname + '/public'));
+		app.use(function(req, res, next){
+			var session = req.session;
+			next();
+		});
 });
 
 // App configure having Two seperate Enviroment 
@@ -29,11 +39,11 @@ app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 app.configure('production', function(){
 app.use(express.errorHandler());
 });
-// Routes
+// Routes are separeted and kept in a different route folder having the GET/POSTs
 //app.get('/', routes.index);
 require('./routes/index')(app);
 require('./routes/user')(app);
-
+require('./routes/session')(app);
 app.listen(3000, function(){
 console.log("Express server listening on port %d in %s mode", app.port,
 app.settings.env);
--- Express/routes/user.js
+++ Express/routes/user.js
@@ -4,47 +4,56 @@
  by a MongoDB out put which is also a JSON 
 */
 var users = require('../data/user');
+var expressValidator = require('express-validator'); // Need to install this 
 
 module.exports = function(app) {
 
-app.get('/users', function(req, res){
-		console.log("Inside the user List in router This is Suppose to Render Views from /views/user/index");
-		console.log("json Object is " + require('util').inspect(users, {depth:null}));
+  app.get('/users', function(req, res){
+		console.log("DEBUG:/GET/USERS:Inside the user List in router This is Suppose to Render Views from /views/user/index");
+		console.log("DEBUG:/GET/USERS:json Object is " + require('util').inspect(users, {depth:null}));
 		res.render('users/index', {title: 'Users', users: users});
-});
+  });
 
-app.get('/users/new', function(req, res) {
+  app.get('/users/new', function(req, res) {
+	console.log("DEBUG:/GET/NEW: Inside new function ");
 	res.render('users/new', {title: "New User"});
-});
+  });
 
 
-app.get('/users/:name', function(req, res, next){
-	console.log("Inside the user name in router");
+  app.get('/users/:name', function(req, res, next){
+	console.log("DEBUG:/GET/USERSNAME: Inside the users name in router ");
 	var user = users[req.params.name];
 	// this will print the JSON object 
-	console.log("json Object is " + require('util').inspect(user, {depth:null}));
+	console.log("DEBUG:/GET/USERSNAME: json Object is \n " + require('util').inspect(user, {depth:null}));
 	if (user) {
 		res.render('users/profile', {title: 'User profile', user: user});
 	} 
 	else {
 		next();
 	}
-});
+  });
 
 
-app.post('/users', function(req, res) {
-console.log("Inside the delete in router");
+  app.post('/users', function(req, res) {
+	console.log("DEBUG:/POST/USERS: Called post for New user Submission ");
+	console.log(req.body);
+	//req.assert('username', 'Please enter a name').notEmpty();
+   
+	
 	if (users[req.body.username]) {
+		console.log("DEBUG:/POST/USERS:  Checking the New user in the List for new User Submission");
 		res.send('Conflict', 409);
 	} 
 	else{
 		users[req.body.username] = req.body;
+		console.log("DEBUG:/POST/USERS: User is a New One so Lets redirect to localhost/users");
 		res.redirect('/users');
 	}
-});
+	
+  });
 
 
-app.del('/users/:name', function(req, res, next) {
+  app.del('/users/:name', function(req, res, next) {
 	console.log("Inside the delete in router");
 	if (users[req.params.name]) {
 		delete users[req.params.name];
@@ -54,6 +63,6 @@ app.del('/users/:name', function(req, res, next) {
 	{
 		next();
 	}
-});
+  });
 
 };
